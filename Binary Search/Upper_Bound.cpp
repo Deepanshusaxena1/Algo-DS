f760740f4ad29bec4f836d7341bc5574f2a86875
Deepanshu <saxenad765@gmail.com>
2020-08-18 15:06:00 +0530

    Added Upper_Bound

---
+++ Binary Search/Upper_Bound.cpp
@@ -0,0 +1,33 @@
+#include "bits/stdc++.h"
+using namespace std;
+int Upper_Bound(vector<int> a, int s)
+{
+    int l = 0;
+    int r = a.size()-1;
+    int UpperBound = -1;
+    while(l<=r)
+    {
+        int m = l + (r-l)/2;
+        if(a[m]<s)
+            l = m+1;
+        else if(a[m]>s)
+            r = m-1;
+        else
+        {
+            UpperBound = m;
+            l = m + 1;
+        }
+    }
+    return UpperBound;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+        cin >> v[i];
+    int s;
+    cin >> s;
+    cout << Upper_Bound(v,s) << '\n';
+}
