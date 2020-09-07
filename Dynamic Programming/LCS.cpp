5065b234f818320b5c8423b13907789c1f50dc08
Deepanshu <saxenad765@gmail.com>
2020-09-07 23:33:58 +0530

    Updated Longest Common Subsequence

--- Dynamic Programming/LCS.cpp
+++ Dynamic Programming/LCS.cpp
@@ -1,14 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
-int LCSRecursive(string s, string t, int n, int m)
+int LCSLengthRecursive(string s, string t, int n, int m)
 {
     if(n==0 or m==0)
         return 0;
     if(s[n-1]==t[m-1])
-        return 1+LCSRecursive(s,t,n-1,m-1);
-    return max(LCSRecursive(s,t,n-1,m),LCSRecursive(s,t,n,m-1));
+        return 1+LCSLengthRecursive(s,t,n-1,m-1);
+    return max(LCSLengthRecursive(s,t,n-1,m),LCSLengthRecursive(s,t,n,m-1));
 }
-int LCS(string s,string t)
+string LCS(string s,string t)
+{
+    int dp[s.size()+1][t.size()+1];
+    for(int i=0;i<=s.size();i++)
+        dp[i][0] = 0;
+    for(int j=0;j<=t.size();j++)
+        dp[0][j] = 0;
+    for(int i=1;i<=s.size();i++)
+        for(int j=1;j<=t.size();j++)
+            if(s[i-1]==t[j-1])
+                dp[i][j] = 1 + dp[i-1][j-1];
+            else
+                dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+    string _lcs = "";
+    int i = s.size();
+    int j = t.size();
+    while(i>=1 && j>=1)
+    {
+        if(s[i-1] == t[j-1])
+        {
+            _lcs += s[i-1];
+            i--;
+            j--;
+        }
+        else if(dp[i-1][j] == dp[i][j])
+            i--;
+        else
+            j--;
+    }
+    reverse(_lcs.begin(),_lcs.end());
+    return _lcs;
+}
+int LCSLength(string s,string t)
 {
     int dp[s.size()+1][t.size()+1];
     for(int i=0;i<=s.size();i++)
